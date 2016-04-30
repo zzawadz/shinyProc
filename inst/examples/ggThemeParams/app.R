06f3b02c5b5a37a62930b52f317aa15cc33af6a6
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2016-04-30 11:22:12 +0200

    App for ggThemeParams

---
+++ inst/examples/ggThemeParams/app.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(ggplot2)
+
+ui <- shinyUI(fluidPage(
+  
+  titlePanel("Params module for ggplot2"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      ggThemeParamsUI("ggThemes")
+    ),
+    
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+))
+
+server <- shinyServer(function(input, output) {
+  
+  themes = callModule(ggMakeThemeParams, "ggThemes")
+  
+  output$plot <- renderPlot({
+    data("mtcars")
+    
+    p = ggplot(mtcars, aes(mpg, disp)) + geom_point()
+    p = themes$call(p)
+    
+    return(p)
+  })
+})
+
+shinyApp(ui = ui, server = server)
+
